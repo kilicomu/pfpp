08ad95e424ea2dcb0ec47d110e73f082ec71e9d2
Killian Murphy <murphyklc@gmail.com>
2020-04-04 21:21:30 +0100

    Implement better unit tests for computation_time; complete implementation of computation_time (#1)
    * Implement better unit tests for computation_time; complete implementation of computation_time
    
    * Move type check for computation_time arguments to function start
    
    * Improve exception messages and reorder arg checking in computation_time
    
    * Restructure type and value checking in computation_time
    
    * Fix grammatical error
    
    * Simplify access to computation_time argument values
    
    * Simplify conditional in computation_time
    
    * Test more failing types in unit tests
    
    * Remove main guard from module
    
    * Exclude some dev tools files from version control

--- pfpp/equations.py
+++ pfpp/equations.py
@@ -7,9 +7,9 @@ import typing as tp
 
 # ------------------------------------------------------------------------------
 def computation_time(
-    setup_time: int,
-    compute_time: int,
-    finalization_time: int,
+    setup_time: tp.Union[int, float],
+    compute_time: tp.Union[int, float],
+    finalization_time: tp.Union[int, float],
     n_processing_elements: int,
 ) -> float:
     """
@@ -32,15 +32,35 @@ def computation_time(
         finalization_time:     The time in seconds for computation finalization.
         n_processing_elements: The number of processing elements assigned for
                                computation.
-    
     Returns:
         float:                 The total computation time in seconds.
+
+    Raises:
+        TypeError:             If n_processing_elements is not an int; if any
+                               of setup_time, compute_time, finalization_time
+                               are not either int or float.
+        ValueError:            If any of setup_time, compute_time, finalization_
+                               time < 0; if n_processing_elements < 1.
     """
+    for arg, value in locals().items():
+        if arg in ("setup_time", "compute_time", "finalization_time"):
+            error_message = (
+                f"{arg} must be an int or float greater than or equal to 0 - "
+                "it represents times in seconds"
+            )
+            if not isinstance(value, (int, float)):
+                raise TypeError(error_message)
+            if value < 0:
+                raise ValueError(error_message)
+        if arg == "n_processing_elements":
+            error_message = (
+                f"{arg} must be an integer greater than 0 - you cannot compute "
+                "on nonexistent or fractional processing elements"
+            )
+            if not isinstance(value, int):
+                raise TypeError(error_message)
+            if value < 1:
+                raise ValueError(error_message)
     return (
         setup_time + (compute_time / n_processing_elements) + finalization_time
     )
-
-
-# ------------------------------------------------------------------------------
-if __name__ == "__main__":
-    pass
--- tests/test_equations.py
+++ tests/test_equations.py
@@ -4,49 +4,121 @@ test_equations.py
 Unit tests for the equations module of pfpp.
 #############################################################################"""
 import unittest
+
 # ------------------------------------------------------------------------------
 from pfpp import equations as eqn
+
 # ------------------------------------------------------------------------------
 class TestComputationTime(unittest.TestCase):
-    def setup(self):
-        pass
+    """
+    Unit tests for equation 2.1 - computation time.
+    """
+
+    def setUp(self):
+        """
+        Test case set up.
+        """
+        self.failing_literals = ["1", 1 + 1j, (1,), [1]]
 
     def tearDown(self):
-        pass
+        """
+        Test case tear down.
+        """
+        del self.failing_literals
+
+    def test_base_case(self):
+        """
+        Test basic correctness of computed result.
+        """
+        self.assertEqual(eqn.computation_time(1, 1, 1, 1), 3)
 
     def test_setup_time(self):
-        self.assertEqual(eqn.computation_time(1, 1, 1, 1), 3)
+        """
+        Test correctness of varying setup_time parameter.
+        """
         self.assertEqual(eqn.computation_time(2, 1, 1, 1), 4)
 
     def test_compute_time(self):
-        pass
+        """
+        Test correctness of varying compute_time parameter.
+        """
+        self.assertEqual(eqn.computation_time(1, 2, 1, 1), 4)
 
     def test_finalization_time(self):
-        pass
+        """
+        Test correctness of varying finalization_time parameter.
+        """
+        self.assertEqual(eqn.computation_time(1, 1, 2, 1), 4)
 
     def test_n_processing_elements(self):
-        pass
+        """
+        Test correctness of varying n_processing_elements parameter.
+        """
+        self.assertEqual(eqn.computation_time(1, 1, 1, 2), 2.5)
 
     def test_setup_time_type_validity(self):
-        pass
+        """
+        Test setup_time type checking.
+        """
+        for literal in self.failing_literals:
+            with self.assertRaises(TypeError):
+                eqn.computation_time(literal, 1, 1, 1)
 
     def test_setup_time_value_validity(self):
-        pass
+        """
+        Test setup_time value checking.
+        """
+        with self.assertRaises(ValueError):
+            eqn.computation_time(-1, 1, 1, 1)
 
     def test_compute_time_type_validity(self):
-        pass
+        """
+        Test compute_time type checking.
+        """
+        for literal in self.failing_literals:
+            with self.assertRaises(TypeError):
+                eqn.computation_time(1, literal, 1, 1)
 
     def test_compute_time_value_validity(self):
-        pass
+        """
+        Test compute_time value checking.
+        """
+        with self.assertRaises(ValueError):
+            eqn.computation_time(1, -1, 1, 1)
 
     def test_finalization_time_type_validity(self):
-        pass
+        """
+        Test finalization_time type checking.
+        """
+        for literal in self.failing_literals:
+            with self.assertRaises(TypeError):
+                eqn.computation_time(1, 1, literal, 1)
 
     def test_finalization_time_value_validity(self):
-        pass
+        """
+        Test finalization time value checking.
+        """
+        with self.assertRaises(ValueError):
+            eqn.computation_time(1, 1, -1, 1)
 
     def test_n_processing_elements_type_validity(self):
-        pass
+        """
+        Test n_processing_elements type checking.
+        """
+        for literal in self.failing_literals:
+            with self.assertRaises(TypeError):
+                eqn.computation_time(1, 1, 1, literal)
+        # Special case for n_processing_elements, as it should throw an error
+        # with a float:
+        with self.assertRaises(TypeError):
+            eqn.computation_time(1, 1, 1, 1.5)
 
     def test_n_processing_elements_value_validity(self):
-        pass
+        """
+        Test n_processing_elements value checking.
+        """
+        with self.assertRaises(ValueError):
+            eqn.computation_time(1, 1, 1, 0)
+
+        with self.assertRaises(ValueError):
+            eqn.computation_time(1, 1, 1, -1)
